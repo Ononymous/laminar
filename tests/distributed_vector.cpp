e2953f7c48cfdc0235b57d4b45260a2257a2fea8
Ononymous <gtamada@ucsb.edu>
2025-01-19 18:08:04 -0800

    distributed vector work!!

--- tests/distributed_vector.cpp
+++ tests/distributed_vector.cpp
@@ -1,7 +1,9 @@
 #include "../df_interface.h"
 #include "type_system/ts_type.h"
-#include "type_system/types/ts_primitive.h"
 #include "type_system/types/ts_array.h"
+#include "type_system/types/ts_matrix.h"
+#include "type_system/types/ts_primitive.h"
+#include "type_system/types/ts_string.h"
 
 #include <iostream>
 #include <string>
@@ -13,73 +15,33 @@ int main() {
     system("sudo find . -name \"lmr*\" -delete");
     laminar_reset(); /* reset setup data structures */
     set_host(1);
-    add_host(1, "localhost", "/cspot-device-namespace/");
+    add_host(1, "localhost", "/home/ubuntu/laminar/build/bin");
     laminar_init();
 
-    // add a node that does nothing
-    add_node(ns, 1, 1, {DF_CUSTOM, VECTOR_DOT_PRODUCT});
-
-    // single input
-    add_operand(ns, 1, 2); // a
-    add_operand(ns, 1, 3); // b
+    const struct df_operation parse = {DF_INTERNAL, DF_INTERNAL_NOOP};
+    add_node(ns, 1, 1, parse);
+    add_operand(ns, 1, 2);
 
     subscribe(ns, 1, 0, ns, 2);
-    subscribe(ns, 1, 1, ns, 3);
-
     laminar_setup();
 
-    std::cout << "Vinayak1" << std::endl;
+    uint8_t array[] = {1, 2, 3};
+    struct ts_value* operand_value = value_from_unsigned_byte_array(array, 3);
+    operand op(operand_value, 1);
+    write_value(&op.operand_value);
+    fire_operand(ns, 2, &op);
+    value_deep_delete(operand_value);
 
-    // firing array
-    int32_t a[] = {1, 2, 3};
-    int32_t b[] = {2, 3, 4};
-    struct ts_value* operand_value1 = value_from_integer_array(a, 3);
-    std::cout << "V1" << std::endl;
-    for(int i = 0; i < 3; i++){
-        std::cout << ((int*)(operand_value1->value.ts_array.value))[i] << std::endl;
-    }
-    operand op1(operand_value1, 1);
-    for(int i = 0; i < 3; i++){
-        std::cout << ((int*)(op1.operand_value.value.ts_array.value))[i] << std::endl;
-    }
-    if(!write_value(&op1.operand_value)){
-        std::cout << "Issue with write value" << std::endl;
-    }
-    std::cout << "V2" << std::endl;
-    for(int i = 0; i < 3; i++){
-        std::cout << ((int*)(op1.operand_value.value.ts_array.value))[i] << std::endl;
-    }
-    fire_operand(ns, 2, &op1);
-    value_deep_delete(operand_value1);
-
-
-    struct ts_value* operand_value2 = value_from_integer_array(b, 3);
-    std::cout << "V1" << std::endl;
-    for(int i = 0; i < 3; i++){
-        std::cout << ((int*)(operand_value2->value.ts_array.value))[i] << std::endl;
-    }
-    operand op2(operand_value2, 1);
-    for(int i = 0; i < 3; i++){
-        std::cout << ((int*)(op2.operand_value.value.ts_array.value))[i] << std::endl;
-    }
-    if(!write_value(&op2.operand_value)){
-        std::cout << "Issue with write value" << std::endl;
-    }
-    std::cout << "V2" << std::endl;
-    for(int i = 0; i < 3; i++){
-        std::cout << ((int*)(op2.operand_value.value.ts_array.value))[i] << std::endl;
-    }
-    fire_operand(ns, 3, &op2);
-    value_deep_delete(operand_value2);
-
-    std::cout << "Animesh2" << std::endl;
-
-    // get value from woof
     operand result;
     get_result(ns, 1, &result, 1);
-    std::cout << result.operand_value.type << std::endl;
-    std::cout << result.itr << std::endl;
-    std::cout << result.operand_value.value.ts_int << std::endl;
+    ts_value* loaded_result = load_value(&result.operand_value);
+
+    uint8_t result_array[3];
+    get_unsigned_byte_array(result_array, loaded_result);
+
+    for(int i = 0; i < sizeof(result_array); i++){
+        std::cout << (int)result_array[i] << std::endl;
+    }
 
     return 0;
 }
